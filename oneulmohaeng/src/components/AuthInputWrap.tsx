60c68875081cd29baf82b18e1ed32c5e2d5ed22b
soyeonkim0227 <soyeonkim0227@naver.com>
2024-06-26 08:50:04 +0900

    ADD :: 인풋박스 컴포넌트화

--- oneulmohaeng/src/components/AuthInputWrap.tsx
+++ oneulmohaeng/src/components/AuthInputWrap.tsx
@@ -1,78 +1,103 @@
-import React from "react";
 import styled from "styled-components";
+import passwordhideIcon from "../images/password_hide_icon.svg";
 
 interface InputProps {
-  word: string;
-  type: string;
-  placeholder: string;
-  image: string;
-  password: boolean;
+    type: string;
+    placeholder: string;
+    image: string;
+    password: boolean;
+    check: boolean;
 }
 
 const AuthInputWrap = (props: InputProps) => {
-  return (
-    <>
-      <Word>{props.word}</Word>
-      <InputWrap image={props.image} password={props.password}>
-        <InputBox type={props.type} placeholder={props.placeholder}></InputBox>
-      </InputWrap>
-    </>
-  );
+    return (
+        <>
+            <Wrap>
+                <InputWrap image={props.image} password={props.password}>
+                    <InputBox
+                        type={props.type}
+                        placeholder={props.placeholder}
+                    ></InputBox>
+                </InputWrap>
+                {props.check && (
+                    <CheckButton>
+                        {props.type === "email" ? "발송" : "중복확인"}
+                    </CheckButton>
+                )}
+            </Wrap>
+        </>
+    );
 };
 
-const InputWrap = styled.div<{ image: string; password: boolean }>`
-  border: 1px solid #e0e0e0;
-  border-radius: 5px;
-  width: 400px;
-  height: 50px;
-  padding: 0 16px;
-  font-size: 16px;
-  margin: 4px 35px 32px 35px;
-
-  display: flex;
-  align-items: center;
-
-  &::before {
-    content: "";
-    display: inline-block;
-    background: url(${(props) => props.image}) center center no-repeat;
-    background-size: cover;
-    object-fit: contain;
-    width: ${(props) => (props.password ? `14px` : `16px`)};
-    height: ${(props) => (props.password ? `17px` : `12px`)};
-  }
-
-  &::after {
-    content: "";
-    display: ${(password) => (password ? `inline-block` : `none`)};
-
-    background: url(image) center center no-repeat;
-    background-size: cover;
-    object-fit: contain;
-    width: 16px;
-    height: 100%;
-  }
+const CheckButton = styled.button`
+    background-color: #F7BBCF;
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
+    color: white;
+    border-radius: 5px;
+    border: none;
+    padding: 0 10px 0 10px;
+    height: 50px;
+    word-break: keep-all;
+    font-size: 16px;
+    font-weight: bold;
 `;
 
-const Word = styled.p`
-  font-weight: bold;
-  display: flex;
-  padding: 0 35px;
-  margin: 30px 0 0 0;
+const InputWrap = styled.div<{
+    image: string;
+    password: boolean;
+}>`
+    background-color: white;
+    border: 1px solid #e0e0e0;
+    border-radius: 5px;
+    width: 100%;
+    height: 50px;
+    padding: 0 16px;
+    font-size: 16px;
+
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    &::before {
+        content: "";
+        display: inline-block;
+        background: url(${(props) => props.image}) center center no-repeat;
+        background-size: cover;
+        object-fit: contain;
+        width: ${(props) => (props.password ? `14px` : `17px`)};
+        height: ${(props) => (props.password ? `15px` : `12.5px`)};
+    }
+
+    &::after {
+        content: "";
+        display: ${(password) => (password ? `inline-block` : `none`)};
+
+        background: url(${() => passwordhideIcon}) center center no-repeat;
+        background-size: cover;
+        object-fit: contain;
+        width: ${(props) => props.password && `19px`};
+        height: ${(props) => props.password && `17px`};
+        margin-left: auto;
+    }
 `;
 
 const InputBox = styled.input`
-  outline: none;
-  border: none;
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  margin-left: 12px;
+    outline: none;
+    border: none;
+    width: 100%;
+    height: 100%;
 
-  &::placeholder {
-    color: #7f7f7f;
-    font-size: 16px;
-  }
+    &::placeholder {
+        color: #7f7f7f;
+        font-size: 16px;
+    }
+`;
+
+const Wrap = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    padding-top: 5px;
 `;
 
 export default AuthInputWrap;
