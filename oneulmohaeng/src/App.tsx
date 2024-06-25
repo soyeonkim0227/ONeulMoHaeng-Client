bcc38e7de8ba4ef434f0e69f85abb89f6bd4514a
soyeonkim0227 <soyeonkim0227@naver.com>
2024-06-26 08:55:36 +0900

    UPDATE :: path 지정 및 코드 정리

--- oneulmohaeng/src/App.tsx
+++ oneulmohaeng/src/App.tsx
@@ -3,16 +3,22 @@ import Feed from "./pages/Feed";
 import Login from "./pages/Login";
 import MyDiary from "./pages/MyDiary";
 import MyPage from "./pages/MyPage";
+import WritingDiary from "./pages/WritingDiary";
+import DiaryDetail from "./pages/DiaryDetail";
+import UserPage from "./pages/UserPage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/my" element={<MyDiary />} />
-      <Route path="/feed" element={<Feed />} />
-      <Route path="/my-page" element={<MyPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/my-diary" element={<MyDiary />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/my-page" element={<MyPage />} />
+            <Route path="/write" element={<WritingDiary />} />
+            <Route path="/diary-id" element={<DiaryDetail />} />
+            <Route path="/user-id" element={<UserPage />} />
+        </Routes>
+    );
 }
 
 export default App;
