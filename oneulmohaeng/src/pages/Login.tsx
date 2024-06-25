9ddf51c43fb34a48a6ae318f9b867f8984ba683e
soyeonkim0227 <soyeonkim0227@naver.com>
2024-06-26 08:51:38 +0900

    ADD :: 로그인/회원가입 페이지 퍼블리싱

--- oneulmohaeng/src/pages/Login.tsx
+++ oneulmohaeng/src/pages/Login.tsx
@@ -1,210 +1,208 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import logo from "../images/oneulmohaeng_logo.svg";
 import loginPhoto from "../images/login_photo.svg";
 import googleLogo from "../images/google_logo.svg";
 import kakaoLogo from "../images/kakao_logo.svg";
 import AuthForm from "../components/AuthForm";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [tab, setTab] = useState(0);
-  const menuArr = [
-    { title: "로그인", content: "로그인 창입니다." },
-    { title: "회원가입", content: "회원가입 창입니다." },
-  ];
+    const [tab, setTab] = useState(0);
+    const menuArr = [
+        { title: "로그인", content: "로그인 창입니다." },
+        { title: "회원가입", content: "회원가입 창입니다." },
+    ];
 
-  const selectMenuHandler = (index: number) => {
-    setTab(index);
-  };
+    const selectMenuHandler = (index: number) => {
+        setTab(index);
+    };
 
-  return (
-    <Wrap>
-      <LogoWrap>
-        <img src={logo} />
-      </LogoWrap>
+    return (
+        <Wrap>
+            <LogoWrap>
+                <img src={logo} />
+            </LogoWrap>
 
-      <ContentWrap>
-        <Content>
-          <TextBody>
-            <Text>
-              <PinkText>오늘모행</PinkText>나요?
-            </Text>
-            <Text>
-              <PinkText>오늘모행</PinkText>는지 기록해 보세요!
-            </Text>
-          </TextBody>
-          <img src={loginPhoto} />
-        </Content>
+            <ContentWrap>
+                <Content>
+                    <TextBody>
+                        <Text>
+                            <PinkText>오늘모행</PinkText>나요?
+                        </Text>
+                        <Text>
+                            <PinkText>오늘모행</PinkText>는지 기록해 보세요!
+                        </Text>
+                    </TextBody>
+                    <img src={loginPhoto} />
+                </Content>
 
-        <TabWrap>
-          <TabMenu>
-            {menuArr.map((ele, index) => {
-              return (
-                <li
-                  key={index}
-                  style={
-                    tab == index
-                      ? { fontWeight: "bold" }
-                      : { fontWeight: "normal" }
-                  }
-                  className={tab === index ? "focused" : "submenu"}
-                  onClick={() => selectMenuHandler(index)}
-                >
-                  {ele.title}
-                </li>
-              );
-            })}
-          </TabMenu>
+                <TabWrap>
+                    <TabMenu>
+                        {menuArr.map((ele, index) => {
+                            return (
+                                <li
+                                    key={index}
+                                    className={
+                                        tab === index ? "focused" : "submenu"
+                                    }
+                                    onClick={() => selectMenuHandler(index)}
+                                >
+                                    {ele.title}
+                                </li>
+                            );
+                        })}
+                    </TabMenu>
 
-          <TabContent>
-            <AuthForm type={menuArr[tab].title}></AuthForm>
-            <SubmitButton>{menuArr[tab].title}</SubmitButton>
+                    <TabContent>
+                        <AuthForm type={menuArr[tab].title}></AuthForm>
+                        <Link to={"/my-diary"}>
+                            <SubmitButton>{menuArr[tab].title}</SubmitButton>
+                        </Link>
 
-            <OauthWrap>
-              <OauthText>다른 계정으로 로그인하기</OauthText>
-              <IconWrap>
-                <img src={googleLogo} />
-                <img src={kakaoLogo} />
-              </IconWrap>
-            </OauthWrap>
-          </TabContent>
-        </TabWrap>
-      </ContentWrap>
-    </Wrap>
-  );
+                        <OauthWrap>
+                            <OauthText>다른 계정으로 로그인하기</OauthText>
+                            <IconWrap>
+                                <img src={googleLogo} />
+                                <img src={kakaoLogo} />
+                            </IconWrap>
+                        </OauthWrap>
+                    </TabContent>
+                </TabWrap>
+            </ContentWrap>
+        </Wrap>
+    );
 };
 
 const IconWrap = styled.div`
-  display: flex;
-  gap: 32px;
-  justify-content: center;
+    display: flex;
+    gap: 32px;
+    justify-content: center;
 `;
 
 const OauthWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 32px 0;
-  gap: 25px;
+    display: flex;
+    flex-direction: column;
+    margin: 30px 0;
+    gap: 25px;
 `;
 
 const OauthText = styled.p`
-  font-size: 16px;
-  color: #7f7f7f;
-  margin: 0;
+    font-size: 16px;
+    color: #7f7f7f;
+    margin: 0;
 `;
 
 const TabMenu = styled.ul`
-  background-color: #f4f4f4;
-  display: flex;
-  align-items: center;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-
-  border-radius: 15px;
-  width: 470px;
-  height: 47px;
-  font-size: 18px;
-  cursor: pointer;
-
-  .submenu {
     background-color: #f4f4f4;
-    width: 237px;
-    height: 47px;
     display: flex;
-    justify-items: center;
-    justify-content: center;
     align-items: center;
-    border-radius: 15px 15px 0px 0px;
-    /* padding: 15px 10px; */
-  }
+    list-style: none;
+    padding: 0;
+    margin: 0;
 
-  .focused {
-    width: 237px;
+    border-radius: 15px 15px 0 0;
+    width: 470px;
     height: 47px;
-    color: #f68db0;
-    background-color: white;
-    border-radius: 15px 15px 0px 0px;
-    display: flex;
-    justify-items: center;
-    justify-content: center;
-    align-items: center;
-  }
+    font-size: 18px;
+    cursor: pointer;
+
+    .submenu {
+        width: 237px;
+        height: 47px;
+        font-weight: normal;
+        display: flex;
+        justify-items: center;
+        justify-content: center;
+        align-items: center;
+        border-radius: 15px 15px 0px 0px;
+    }
+
+    .focused {
+        background-color: white;
+        width: 237px;
+        height: 47px;
+        color: #f68db0;
+        font-weight: bold;
+        border-radius: 15px 15px 0px 0px;
+        display: flex;
+        justify-items: center;
+        justify-content: center;
+        align-items: center;
+    }
 `;
 
 const TabContent = styled.div`
-  /* background-color: white; */
-  width: 470px;
-  height: 472px;
-  font-size: 16px;
-  text-align: center;
+    width: 470px;
+    font-size: 16px;
+    text-align: center;
+    padding: 0 35px;
 
-  .login {
-  }
+    #login {
+    }
 
-  .signup {
-  }
+    #signup {
+        width: 700px;
+    }
 `;
 
 const TabWrap = styled.div`
-  background-color: white;
-  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
-  border-radius: 15px;
-  display: flex;
-  flex-direction: column;
+    background-color: white;
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const SubmitButton = styled.button`
-  background-color: #f68db0;
-  border: none;
-  border-radius: 5px;
-  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
-  font-size: 18px;
-  font-weight: bold;
-  color: white;
-  width: 400px;
-  height: 50px;
-  padding: 0;
-  cursor: pointer;
+    background-color: #f68db0;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
+    font-size: 18px;
+    font-weight: bold;
+    color: white;
+    width: 400px;
+    height: 50px;
+    margin-top: 30px;
+    cursor: pointer;
 `;
 
 const Wrap = styled.div`
-  padding: 17px 323px;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
+    padding: 17px 323px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
 `;
 
 const LogoWrap = styled.div``;
 
 const ContentWrap = styled.div`
-  display: flex;
-  flex: 1;
-  align-items: center;
-  gap: 70px;
-  /* margin-top: 230px; */
+    display: flex;
+    flex: 1;
+    /* justify-content: center; */
+    align-items: center;
+    gap: 70px;
 `;
 
 const Content = styled.div`
-  /* flex: 1; */
-  /* height: 100%; */
-  font-size: 25px;
-  display: flex;
-  flex-direction: column;
+    font-size: 25px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const TextBody = styled.div`
-  margin-bottom: 79px;
+    margin-bottom: 79px;
 `;
 
 const Text = styled.div`
-  display: flex;
+    display: flex;
 `;
 
 const PinkText = styled.p`
-  color: #f68db0;
-  font-weight: bold;
-  margin: 0;
+    color: #f68db0;
+    font-weight: bold;
+    margin: 0;
 `;
 
 export default Login;
